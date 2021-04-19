ae6fda708b22e7f6beaf4b54c92c90be681add9d
Peter Nguyen <p3t3rn9uy3n@gmail.com>
2021-04-18 22:38:18 -0500

    added lib folder and intern engineer and manager.js files

---
+++ index.js
@@ -0,0 +1,40 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util')
+const manager = require('.lib/manager');
+const engineer = require('.lib/engineer');
+const intern = require('.lib/intern');
+
+// manager question array
+const questions = [
+    {
+        type: "input",
+        message: "What is the team manager's name?",
+        name: "manager"
+    },
+    {
+        type: "input",
+        message: "What is the team manager's ID?",
+        name: "managerid"
+    },
+    {
+        type: "input",
+        message: "What is the team manager's email?",
+        name: "manageremail"
+    },
+    {
+        type:"input",
+        message: "What is the team manager's office number?",
+        name: "managernumber",
+    },
+    {
+        type: "list",
+        message: "Which type of team member would you like to add?",
+        name: "teammember",
+        choices: [
+            "Engineer",
+            "Intern",
+            "I do not want to add anymore members"
+        ]
+    },
+];
